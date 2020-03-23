aa296f8df619d8a0a9a250d6865dd2e6cc061ea1
Edgar Goetzendorff <hello@dated.fun>
2020-03-23 10:05:06 +0100

    refactor: add thank you notice to disclaimer

--- src/components/modals/DisclaimerModal.js
+++ src/components/modals/DisclaimerModal.js
@@ -3,13 +3,22 @@ module.exports = {
     <ModalConfirmation
       container-classes="max-w-md"
       title="Disclaimer"
-      note="The information presented by this plugin is based on the data available on https://arkdelegates.live and has been prepared for informational purposes only."
       cancel-button="Cancel"
       continue-button="I understand"
       @cancel="emitCancel"
       @close="emitCancel"
       @continue="emitConfirm"
-    />
+    >
+      <div class="mb-6 text-grey-darker text-lg">
+        <p class="mb-3">
+          The information presented by this plugin is based on the data available on https://arkdelegates.live and has been prepared for informational purposes only.
+        </p>
+
+        <p>
+          Special thanks to delegates <span class="font-semibold">deadlock</span> and <span class="font-semibold">itsanametoo</span> for building and maintaining https://arkdelegates.live
+        </p>
+      </div>
+    </ModalConfirmation>
   `,
 
   props: {
